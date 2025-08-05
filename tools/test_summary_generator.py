cc6b780af0db496b19f7fc300dc34fa46e9440bd
isharaju <49925748+isharaju@users.noreply.github.com>
2025-08-04 22:31:15 -0400

    Delete tools directory

--- tools/test_summary_generator.py
+++
@@ -1,8 +0,0 @@
-from tools.summary_generator import generate_summary
-
-def test_summary_generator():
-    result = generate_summary("Trial A: Eligible\nTrial B: Ineligible")
-    print(result)
-
-if __name__ == "__main__":
-    test_summary_generator()
